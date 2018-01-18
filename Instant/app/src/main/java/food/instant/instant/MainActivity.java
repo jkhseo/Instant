5fc7ed6c066ef86f20985d8ec7c170ee674156c9
Peter Keppeler <keppeler@iastate.edu>
2018-01-18 15:05:24 -0600

    Added four buttons, and linked them to their respective views, for Customer, Admin, Vendor, and New User

--- Instant/app/src/main/java/food/instant/instant/MainActivity.java
+++ Instant/app/src/main/java/food/instant/instant/MainActivity.java
@@ -1,7 +1,9 @@
 package food.instant.instant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*Opens the new user activity*/
+    public void startNewUserActivity(View view)
+    {
+        Intent myIntent = new Intent(MainActivity.this, NewUserActivity.class);
+        MainActivity.this.startActivity(myIntent);
+    }
+
+    /*Opens the Vendor activity*/
+    public void startVendorActivity(View view)
+    {
+        Intent myIntent = new Intent(MainActivity.this, VendorActivity.class);
+        MainActivity.this.startActivity(myIntent);
+    }
+
+    /*Opens the Admin activity*/
+    public void startAdminActivity(View view)
+    {
+        Intent myIntent = new Intent(MainActivity.this, AdminActivity.class);
+        MainActivity.this.startActivity(myIntent);
+    }
+
+    /*Opens the Customer activity*/
+    public void startCustomerActivity(View view)
+    {
+        Intent myIntent = new Intent(MainActivity.this, CustomerActivity.class);
+        MainActivity.this.startActivity(myIntent);
     }
 }
