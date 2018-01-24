86ab23096f828dd1a572731e22388e055f8b407a
Peter Keppeler <keppeler@iastate.edu>
2018-01-23 18:16:02 -0600

    Created a mockup-up for a login activity

--- Instant/app/src/main/java/food/instant/instant/NewUserActivity.java
+++ Instant/app/src/main/java/food/instant/instant/NewUserActivity.java
@@ -1,7 +1,9 @@
 package food.instant.instant;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class NewUserActivity extends AppCompatActivity {
 
@@ -10,4 +12,5 @@ public class NewUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
     }
+
 }
