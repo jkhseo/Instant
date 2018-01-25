9675b12951e29b17ec79a6517eee580c3abd86b6
adegala <adegala@iastate.edu>
2018-01-25 15:34:26 -0600

    Adding more files for the example database

--- Instant/app/src/main/java/food/instant/instant/Message.java
+++ Instant/app/src/main/java/food/instant/instant/Message.java
@@ -1,8 +1,10 @@
 package food.instant.instant;
 
-/**
- * Created by adamdegala on 1/25/18.
- */
+import android.content.Context;
+import android.widget.Toast;
 
 public class Message {
+    public static void message(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
 }
