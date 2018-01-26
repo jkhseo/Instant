0b5ceb4bef9aa2b8d67e349d35f23184c0c24681
Peter Keppeler <keppeler@iastate.edu>
2018-01-26 08:55:52 -0600

    Removed action bar from appearing in Login Activity

--- Instant/app/src/main/java/food/instant/instant/NewUserActivity.java
+++ Instant/app/src/main/java/food/instant/instant/NewUserActivity.java
@@ -3,6 +3,7 @@ package food.instant.instant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.Toast;
 
 public class NewUserActivity extends AppCompatActivity {
@@ -10,6 +11,7 @@ public class NewUserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_new_user);
     }
 
