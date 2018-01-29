90d44fd729a87f2c932517cdf5b8951998cad3ff
adegala <adegala@iastate.edu>
2018-01-29 11:41:52 -0600

    Added untracked files for new Ording viewing

--- Instant/app/src/main/java/food/instant/instant/ViewOrder.java
+++ Instant/app/src/main/java/food/instant/instant/ViewOrder.java
@@ -6,24 +6,28 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class ViewOrder extends AppCompatActivity {
 
+    myDbAdapter helper;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_order);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        helper = new myDbAdapter(this);
+
+        String data = helper.getData();
+
+        final TextView order = (TextView) findViewById(R.id.order);
+        order.setText(data);
+
+
     }
 
 }
