324ec0ae7066596d30841f4a4769a51ed02f1f86
Peter Keppeler <keppeler@iastate.edu>
2018-01-29 14:31:22 -0600

    Added click listener for login button, and returns that the button was clicked to the console

--- Instant/app/src/main/java/food/instant/instant/NewUserActivity.java
+++ Instant/app/src/main/java/food/instant/instant/NewUserActivity.java
@@ -3,16 +3,37 @@ package food.instant.instant;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class NewUserActivity extends AppCompatActivity {
+
+    EditText etUsername;
+    EditText etPassword;
+
+    Button ibLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_new_user);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        ibLogin = findViewById(R.id.ibLogin);
+
+        ibLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Login", "Login button was hit!");
+            }
+        });
     }
 
 }
