4f1432d2a87e85d3813bfa57efc593426b10dfe0
Peter Keppeler <keppeler@iastate.edu>
2018-02-04 18:10:28 -0600

    Added a SavedSharedPreference class to save login state, and current user info. Changed navgivation drawer to display different options based on login status.

--- Instant/app/src/main/java/food/instant/instant/MainActivity.java
+++ Instant/app/src/main/java/food/instant/instant/MainActivity.java
@@ -1,5 +1,6 @@
 package food.instant.instant;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -7,13 +8,16 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        NavigationView mNavigationView = findViewById(R.id.nav_view);
+        final NavigationView mNavigationView = findViewById(R.id.nav_view);
+
+        //helper method to hide login/logout buttons
+        checkUi(mNavigationView, MainActivity.this);
+
         mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -37,10 +45,22 @@ public class MainActivity extends AppCompatActivity {
                     case(R.id.nav_login):
                         Intent loginIntent = new Intent(MainActivity.this, NewUserActivity.class);
                         MainActivity.this.startActivity(loginIntent);
+                        break;
+                    case(R.id.nav_logout):
+                        SaveSharedPreference.clearUserName(MainActivity.this);
+                        checkUi(mNavigationView, MainActivity.this);
+                        break;
                 }
                 return true;
             }
         });
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        final NavigationView mNavigationView = findViewById(R.id.nav_view);
+        checkUi(mNavigationView, MainActivity.this);
     }
 
     @Override
@@ -78,5 +98,28 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent myIntent = new Intent(MainActivity.this, CustomerActivity.class);
         MainActivity.this.startActivity(myIntent);
+    }
+
+    private void checkUi(NavigationView mNavigationView, Context cxt)
+    {
+        TextView welcomeMsg = mNavigationView.getHeaderView(0).findViewById(R.id.nav_header_message);
+        //if user is logged out
+        if(SaveSharedPreference.getUserName(cxt).length() == 0)
+        {
+            mNavigationView.getMenu().findItem(R.id.nav_logout).setVisible(false);
+            mNavigationView.getMenu().findItem(R.id.nav_account).setVisible(false);
+            mNavigationView.getMenu().findItem(R.id.nav_settings).setVisible(false);
+            mNavigationView.getMenu().findItem(R.id.nav_login).setVisible(true);
+            welcomeMsg.setText("Welcome");
+        }
+        //else user is logged in
+        else
+        {
+            mNavigationView.getMenu().findItem(R.id.nav_logout).setVisible(true);
+            mNavigationView.getMenu().findItem(R.id.nav_account).setVisible(true);
+            mNavigationView.getMenu().findItem(R.id.nav_settings).setVisible(true);
+            mNavigationView.getMenu().findItem(R.id.nav_login).setVisible(false);
+            welcomeMsg.setText("Welcome, " + SaveSharedPreference.getUserName(cxt));
+        }
     }
 }
--- Instant/app/src/main/java/food/instant/instant/NewUserActivity.java
+++ Instant/app/src/main/java/food/instant/instant/NewUserActivity.java
@@ -1,11 +1,11 @@
 package food.instant.instant;
 
-import android.content.Intent;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -13,27 +13,82 @@ import android.widget.Toast;
 
 public class NewUserActivity extends AppCompatActivity {
 
-    EditText etUsername;
-    EditText etPassword;
+    private static final String TAG = "NewUserActivity";
 
-    Button ibLogin;
+    private Context c;
+
+    private EditText etUsername;
+    private EditText etPassword;
+
+    private Button bLogin;
+
+    private ImageButton ibClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        c = this;
+
         setContentView(R.layout.activity_new_user);
 
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
-        ibLogin = findViewById(R.id.ibLogin);
+        bLogin = findViewById(R.id.ibLogin);
+        ibClose = findViewById(R.id.ibClose);
 
-        ibLogin.setOnClickListener(new View.OnClickListener() {
+        ibClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("Login", "Login button was hit!");
+                finish();
             }
         });
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "Login button was hit!");
+
+                String username = etUsername.getText() + "";
+                String password = etPassword.getText() + "";
+
+                if(username.length() == 0 && password.length() != 0)
+                {
+                    Toast.makeText(c, "Please enter a username.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if(username.length() != 0 && password.length() == 0)
+                {
+                    Toast.makeText(c, "Please enter a password.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if(username.length() == 0 && password.length() == 0)
+                {
+                    Toast.makeText(c, "Please enter a username and password.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else
+                {
+                    Log.d(TAG, "Username: " + username);
+                    Log.d(TAG, "Password: " + password);
+
+                    SaveSharedPreference.setUserName(c, username);
+                    finish();
+                }
+            }
+        });
+    }
+
+    //this is so that the UI doesnt get hung up during the http request, and instead the task gets done in the background
+    public class Networking extends AsyncTask
+    {
+        public static final int NETWORK_STATE_REGISTER = 1;
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            Log.d(TAG, "doInBackground");
+
+            return null;
+        }
     }
 
 }
